3f586ca381e58812cdf4ef622e0a8dc48da27359
earthperson <ponomarev.dev@gmail.com>
2014-06-14 01:04:16 +0400

    Update notes.sh

--- notes.bat
+++ notes.bat
@@ -6,4 +6,5 @@ REM tracert
 REM subst
 REM mstsc /console /admin
 REM net session
-
+REM netsh interface portproxy add v4tov4 listenport= {целое_число | имя_службы} [[connectaddress=] {IPv4-адрес | имя_узла}] [[connectport=] {целое_число | имя_службы}] [[listenaddress=] {IPv4-адрес| имя_узла}] [[protocol=]tcp]
+REM netsh interface portproxy delete v4tov4 listenport= {целое_число | имя_службы} [[listenaddress=] {IPv4-адрес| имя_узла}] [[protocol=]tcp]
