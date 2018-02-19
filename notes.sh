0fc82be453401d244c2371fd0bf2b551dab8ebfc
earthperson <ponomarev.dev@gmail.com>
2018-02-19 16:42:51 +0300

    Updated notes.sh

--- notes.sh
+++ notes.sh
@@ -1,79 +1,80 @@
-#ping
-#netstat -nlp
-#nslookup
-#ifconfig -a
-#whoami
-#net
-#tee
-#useradd
-#groupadd
-#groups
-#strace -p
-#screen
-#help
-#file
-#linphonec
-#minicom
-#ncdu
-#du -sh
-#strace
-#echo $0
-#php
-#mysql
-#mysqldump
-#httpd
-#pear
-#zf
-#alias
-#dmesg
-#df -h
-#nohup
-#route -n
-#type
-#watch
-#finger
-#tcpdump -vv -n -i eth0 tcp port <port>
-#tcpdump -ieth0 -s0 -w/tmp/dd port 3306 -> wireshark
-#hostname
-#mail
-#dhclient eth0
-#/etc/hosts
 #/etc/host.conf
-#/etc/resolv.conf
-#/etc/network/interfaces
+#/etc/hosts
 #/etc/init.d/networking {start|stop}
-#visudo
-#sudoedit
-#patch
-#locale -a
+#/etc/network/interfaces
+#/etc/resolv.conf
+#alias
 #apropos
-#xargs
-#nc, netcat
-#nmap
-#hexdump -C foo.php
-#dpkg --get-selections | grep <package>
-#update-alternatives --config <name>
-#dpkg-reconfigure 
-#update-rc.d #which is the equivalent of chkconfig
-#png2ico favicon.ico logo16x16.png logo32x32.png
-#ls -lh <filename> | cut -f 5 -d " "
-#git reset[ --soft|hard] HEAD~1
-#git reflog
-#m-a prepare
-#mount -t vboxsf share mount_point
-#split -n <n> file
+#apt-get install fonts-inconsolata
+#apt-get install msttcorefonts
+#cal
 #cat x* #Better start typing cat x, than press Esc and then * - you'll see the expanded order of files and can rearrange.
-#less /etc/services
+#crontab -e
+#df -h
+#dhclient eth0
+#dmesg
+#dpkg --get-selections | grep <package>
+#dpkg-reconfigure 
+#du -sh
+#echo $0
 #export #declare #let #local #printenv #set #unset #Михаэль Кофлер. Весь Linux. Локальные и глобальные переменные (переменные среды). Стр. 440
-#sed [опции] команда [< источник > цель] #Михаэль Кофлер. Весь Linux. Алфавитный перечень команд. Стр. 513
+#file
+#finger
+#git checkout --orphan <feature_branch>; git rm --cached -r .; git add <source>; git commit -m "Start new feature branch"; git push origin <feature_branch>
 #git checkout --ours <filename>
 #git checkout --theirs <filename>
-#apt-get install msttcorefonts
-#apt-get install fonts-inconsolata
-#yum install kernel-devel-$(uname -r) kernel-headers-$(uname -r) gcc
-#sudo dd if=/dev/zero of=/bigemptyfile bs=4096k; sudo rm -rf /bigemptyfile #http://superuser.com/questions/529149/how-to-compact-virtualboxs-vdi-file-size
-#cal
+#git reflog
+#git reset[ --soft|hard] HEAD~1
+#groupadd
+#groups
+#help
+#hexdump -C foo.php
+#hostname
+#httpd
+#ifconfig -a
 #last reboot
+#less /etc/services
+#linphonec
+#locale -a
+#ls -lh <filename> | cut -f 5 -d " "
+#m-a prepare
+#mail
+#minicom
+#mount -t vboxsf share mount_point
+#mysql
+#mysqldump
+#nc, netcat
+#ncdu
+#net
+#netstat -nlp
+#nmap
+#nohup
+#nslookup
+#patch
+#pear
+#php
+#ping
+#png2ico favicon.ico logo16x16.png logo32x32.png
+#route -n
+#screen
+#sed [опции] команда [< источник > цель] #Михаэль Кофлер. Весь Linux. Алфавитный перечень команд. Стр. 513
+#split -n <n> file
+#strace
+#strace -p
+#sudo dd if=/dev/zero of=/bigemptyfile bs=4096k; sudo rm -rf /bigemptyfile #http://superuser.com/questions/529149/how-to-compact-virtualboxs-vdi-file-size
+#sudoedit
 #tar -cvjf file.tar.bz2 list_of_files
 #tar -xvjf file.tar.bz2
-#git checkout --orphan <feature_branch>; git rm --cached -r .; git add <source>; git commit -m "Start new feature branch"; git push origin <feature_branch>
+#tcpdump -ieth0 -s0 -w/tmp/dd port 3306 -> wireshark
+#tcpdump -vv -n -i eth0 tcp port <port>
+#tee
+#type
+#update-alternatives --config <name>
+#update-rc.d #which is the equivalent of chkconfig
+#useradd
+#visudo
+#watch
+#whoami
+#xargs
+#yum install kernel-devel-$(uname -r) kernel-headers-$(uname -r) gcc
+#zf
