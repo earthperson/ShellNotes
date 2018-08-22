550d256b9c75021283c87ef47efce5c05bce0190
earthperson <ponomarev.dev@gmail.com>
2018-08-22 13:02:15 +0300

    Updated notes.bat

--- notes.bat
+++ notes.bat
@@ -1,27 +1,27 @@
-REM path
-REM set
-REM sc
+REM dxdiag
 REM ipconfig
-REM tracert
-REM subst
 REM msconfig
 REM mstsc /console /admin
 REM net session
 REM netsh interface portproxy add v4tov4 listenport= {целое_число | имя_службы} [[connectaddress=] {IPv4-адрес | имя_узла}] [[connectport=] {целое_число | имя_службы}] [[listenaddress=] {IPv4-адрес| имя_узла}] [[protocol=]tcp]
 REM netsh interface portproxy delete v4tov4 listenport= {целое_число | имя_службы} [[listenaddress=] {IPv4-адрес| имя_узла}] [[protocol=]tcp]
 REM netsh interface portproxy show all
-REM VBoxManage.exe modifyhd c:\path\to\thedisk.vdi --compact #http://superuser.com/questions/529149/how-to-compact-virtualboxs-vdi-file-size
+REM path
+REM regedit
+REM sc
 REM sdelete -c <physical disk number> #https://technet.microsoft.com/en-us/sysinternals/bb897443.aspx
+REM set
+REM subst
+REM telnet
+REM tracert
+REM VBoxManage.exe modifyhd c:\path\to\thedisk.vdi --compact #http://superuser.com/questions/529149/how-to-compact-virtualboxs-vdi-file-size
 REM Verifying checksums on Windows systems (powershell session)
 REM $file="file"
 REM Add-Type -AssemblyName System.Core
 REM $sha256 = New-Object -TypeName System.Security.Cryptography.sha256CryptoServiceProvider #sha1CryptoServiceProvider
 REM $checksum = [System.BitConverter]::ToString($sha256.ComputeHash([System.IO.File]::ReadAllBytes("$PWD\$file"))).ToLower() -replace '-', ''
 REM echo "Checksum: $checksum"
-REM regedit
-REM dxdiag
 REM Режимы запуска Word (упрощенный, безопасный, макрос)
 REM WINWORD /A
 REM WINWORD /SAFE
 REM WINWORD /M
-REM telnet
